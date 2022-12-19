238a52e95ec04a6b253a10bc9c3af251dbd97c38
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2022-12-18 18:23:35 -0600

    Added the general webpage layout to the html script, added some of the styling to the css script and added the timer and some empty variables to the js script.

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var initialText="Coding Quiz Challenge";
+
+var questionPrompts=["Commonly used data types DO NOT include:","Arrays in JavaScript can be used to store: ____."];
+var answers=["",""];
+var answerKey=[,];
+
+// Selects element by class
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 60;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      timeEl.textContent = " ";
+
+    //   sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
